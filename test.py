f8440a4fa8322731babb3e26d200bb4307f6d20f
Yuntianyi-Chen <yuntianyichen@outlook.com>
2022-10-12 15:02:09 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,27 @@ import glob
 import os
 import shutil
 import subprocess
+from pathlib import Path
 
-from config import APOLLO_ROOT, MAGGIE_ROOT
-from environment.container_settings import get_container_name
+# a= Path.cwd()/"asdasfa.txt.00000"
+#
+# print(Path.cwd()/"asdasfa.txt")
+# print(Path.cwd().joinpath("asdad"))
+# print(Path.home())
+# print(a.stem)
+# print(a.suffix)
+# print(a.parent)
+# print(a.suffixes)
 
-with open(f"{MAGGIE_ROOT}/violation_results.txt", "a") as f:
-    f.write(f"Violation Results: \n")
-    f.write(f"  record_path\n")
+class fff:
+    def __str__(self):
+        return "asdasd"
+    def __repr__(self):
+        return "rere"
+# current_dir = Path.cwd()
+# py_files = list(current_dir.glob('**/*.py'))
+
+a = fff()
+
+print(a.__repr__())
+print(a.__str__())
