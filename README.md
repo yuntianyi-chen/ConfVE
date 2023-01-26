d2caa4b372a3835a78957e9d923eef80602c9744
Yuntianyi-Chen <yuntianyichen@outlook.com>
2023-01-26 15:36:30 -0800

    update requirements.txt

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ ADS Configuration Testing Framework
 1. Replace the file `apollo/docker/scripts/dev_start.sh` with the file under `{project}/data/scripts/apollo_multi_container/dev_start.sh`
 2. Start your container with `./apollo.sh dev_start.sh`, build your apollo system using instructions in the official website
 3. Change the IMPORTANT CONFIGURATION and DIR values in `config.py` according to your environment. Place the project under `DIR_ROOT`
-4. Place initial records under `/data/records`, run `prepare.py`
+4. Place initial record files under `/data/records`, run `prepare.py`
 4. Run `python3 main.py`
--- src/tools/script/stat_config_file.py
+++ src/tools/script/stat_config_file.py
@@ -1,22 +1,20 @@
-import glob
-from optimization_algorithms.genetic_algorithm.ga import ga_init
-from tools.script.config_file_handler.parser_apollo import parser2class
+import os
+from config import DEFAULT_CONFIG_FILE_PATH, PROJECT_ROOT, MODULE_NAME, CONFIG_FILE_NAME
+from config_file_handler.parser_apollo import config_file_parser2obj
 
 if __name__ == '__main__':
+    # print(os.getcwd())
+    # os.chdir(PROJECT_ROOT)
     # module_config_path = f"{APOLLO_ROOT}/modules/{MODULE_NAME}/conf/{MODULE_NAME}_config.pb.txt"
     # module_config_path = f"../data/config_files/{MODULE_NAME}/conf/{MODULE_NAME}_config.pb.txt"
-    module_name = "control"
-
-    conf_files_path = glob.glob(
-        f"C:\\Projects\\Research\\Autonomous Vehicles\\apollo\\apollo\\modules\\{module_name}\\conf" + '/**/calibration_table.pb.txt',
-        recursive=True)
+    # module_name = "control"
 
     # conf_files_path = glob.glob(
-    #     f"C:\\Projects\\Research\\Autonomous Vehicles\\apollo\\apollo\\modules\\{module_name}\\conf" + '/**/*_conf.pb.txt',
+    #     f"C:\\Projects\\Research\\Autonomous Vehicles\\apollo\\apollo\\modules\\{module_name}\\conf" + '/**/calibration_table.pb.txt',
     #     recursive=True)
-    # config_files_path = glob.glob(
-    #     f"C:\\Projects\\Research\\Autonomous Vehicles\\apollo\\apollo\\modules\\{module_name}\\conf" + '/**/*_config.pb.txt',
-    #     recursive=True)
+
+    conf_files_path = [f"{CONFIG_FILE_NAME}"]
+
     all_config_files_path = conf_files_path
     type_stat_dict = {}
     print(len(all_config_files_path))
@@ -24,11 +22,12 @@ if __name__ == '__main__':
         # if "calibration" in config_file:
         # ga_main(module_config_path)
         print(config_file)
-        raw_option_stack, option_tuple_list, option_obj_list, option_num = parser2class(config_file)
+        # raw_option_stack, option_tuple_list, option_obj_list, option_num = parser2class(config_file)
 
-        init_individual_list, generation_limit, option_type_list = ga_init(option_obj_list)
+        # init_individual_list, generation_limit, option_type_list = ga_init(option_obj_list)
+        config_file_obj = config_file_parser2obj(config_file)
 
-        for i in option_type_list:
+        for i in config_file_obj.option_type_list:
             if i not in type_stat_dict.keys():
                 type_stat_dict[i] = 1
             else:
