6214a4dc50d7de4f626aa8478fd7683128de9dd2
yuntianyi-chen <yuntianyichen@outlook.com>
2023-02-22 14:29:06 -0800

    Modify Paths in config.py

--- src/config.py
+++ src/config.py
@@ -1,27 +1,31 @@
 """
 Global configurations for the framework
 """
+from pathlib import Path
 
-###
-# routing request message.header.module name
-# different for different AV_TESTING_APPROACH
-###
 
-# IMPORTANT CONFIGURATION
+
+#######################
+## Customized Config ##
+#######################
+
 OBS_PERCEPTION_FREQUENCY = 25  # scenoRITA/AV-Fuzzer/ADFuzz-10  /  DoppelTest-25
 MAX_RECORD_TIME = 10  # 10/30
 AV_TESTING_APPROACH = "DoppelTest"  # scenoRITA/DoppelTest/AV-Fuzzer/ADFuzz
 MAP_NAME = "san_mateo"  # borregas_ave/sunnyvale_loop/San_Francisco/san_mateo
-CONTAINER_NUM = 5  # 5/10
+CONTAINER_NUM = 1  # 5/10
 OPT_MODE = "T-way"  # GA/DRL/Random/T-way
-
-# t-way testing
-T_STRENGTH_VALUE = 2  # pairwise
 TIME_HOUR_THRESHOLD = 10  # hours
 
 
-#############################################
 
+
+####################
+## Default Config ##
+####################
+
+# t-way testing
+T_STRENGTH_VALUE = 2  # pairwise
 
 # TESTING SETTINGS
 MODULE_NAME = "planning"
@@ -39,6 +43,7 @@ GENERATION_LIMIT = 50  # 10/20/50
 INIT_POP_SIZE = 10  # 60 individuals in each generation
 FITNESS_MODE = "multi_obj"  # emerge/multi_obj
 SELECT_MODE = "nsga2"  # nsga2/ratio
+
 # FOR RATIO SELECT
 SELECT_NUM_RATIO = [7, 2, 1]  # [5, 3, 2]/[7, 2, 1]
 
@@ -47,25 +52,31 @@ DETERMINISM_RERUN_TIMES = 5  # 5/10
 DEFAULT_DETERMINISM_RERUN_TIMES = 10  # 10
 DETERMINISM_CONFIRMED_TIMES = 3  # 3
 
-MODULE_ORACLES = ["RoutingFailure", "PredictionFailure", "PlanningFailure", "CarNeverMoved", "SimControlFailure",
+MODULE_ORACLES = ["RoutingFailure",
+                  "PredictionFailure",
+                  "PlanningFailure",
+                  "CarNeverMoved",
+                  "SimControlFailure",
                   "PlanningGeneratesGarbage"]
 
+INITIAL_EXP_NAME = f"{AV_TESTING_APPROACH}_{MAP_NAME}_{MAX_RECORD_TIME}"
 
 # DIRECTORIES
-DIR_ROOT = '/home/cloudsky/Research/Apollo'
-APOLLO_ROOT = f'{DIR_ROOT}/apollo_v7_deploy'  # apollo_v7_deploy/apollo_7.0
-PROJECT_ROOT = f'{DIR_ROOT}/AV_Config_Testing'
+DIR_ROOT = str(Path(__file__).parent.parent.parent)
+PROJECT_ROOT = str(Path(__file__).parent.parent)
+APOLLO_ROOT = f'{DIR_ROOT}/apollo_v7_deploy'  # apollo_v7_deploy/apollo_7.0/AV_Config_Testing
 
 RECORDS_DIR = f'{PROJECT_ROOT}/data/records'
 FEATURES_CSV_DIR = f'{PROJECT_ROOT}/data/violation_features'
 APOLLO_RECORDS_DIR = f'{APOLLO_ROOT}/records'
 OBS_DIR = f"{APOLLO_ROOT}/modules/tools/perception/obstacles/{MAP_NAME}"
-EXP_GROUP_NAMING_TREE = f"{AV_TESTING_APPROACH}/{MAP_NAME}/{AV_TESTING_APPROACH}_{MAP_NAME}_{MAX_RECORD_TIME}_{OPT_MODE}"
+EXP_GROUP_NAMING_TREE = f"{AV_TESTING_APPROACH}/{INITIAL_EXP_NAME}_{OPT_MODE}"
 EXP_BASE_DIR = f"{PROJECT_ROOT}/data/exp_results/{EXP_GROUP_NAMING_TREE}"
+INITIAL_SCENARIO_RECORD_DIR = f"{DIR_ROOT}/Backup/initial/{INITIAL_EXP_NAME}"
 BACKUP_SAVE_DIR = f'{DIR_ROOT}/Backup/{EXP_GROUP_NAMING_TREE}'
 BACKUP_CONFIG_SAVE_DIR = f"{BACKUP_SAVE_DIR}/config_files"
 BACKUP_RECORD_SAVE_DIR = f"{BACKUP_SAVE_DIR}/records"
-INITIAL_SCENARIO_RECORD_DIR = f"{BACKUP_RECORD_SAVE_DIR}/initial"
+# INITIAL_SCENARIO_RECORD_DIR = f"{BACKUP_RECORD_SAVE_DIR}/initial"
 DEFAULT_RERUN_INITIAL_SCENARIO_RECORD_DIR = f"{BACKUP_RECORD_SAVE_DIR}/initial_default_rerun"
 MAP_DIR = f'{PROJECT_ROOT}/data/maps/{MAP_NAME}'
 MY_SCRIPTS_DIR = f"{APOLLO_ROOT}/scripts/my_scripts"
@@ -92,6 +103,6 @@ APOLLO_VEHICLE_WIDTH = 2.11
 APOLLO_VEHICLE_HEIGHT = 1.48
 APOLLO_VEHICLE_back_edge_to_center = 1.043
 
-OBS_TYPE_DICT = {3: "PEDESTRIAN", 4: "BICYCLE", 5: "VEHICLE"}
+# OBS_TYPE_DICT = {3: "PEDESTRIAN", 4: "BICYCLE", 5: "VEHICLE"}
 
 
