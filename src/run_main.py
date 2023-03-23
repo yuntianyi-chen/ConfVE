bcb5113357beab5f0ba7ec9cb6ace00c43e70ddd
yuntianyi-chen <yuntianyichen@outlook.com>
2023-03-22 21:09:51 -0700

    restructure run_main.py

--- src/run_main.py
+++ src/run_main.py
@@ -17,11 +17,12 @@ def move_file(source_dir, target_dir):
 
 
 if __name__ == '__main__':
+    subprocess.run(f"cd {PROJECT_ROOT} && make dpurge", shell=True)
     subprocess.run("python3 main.py", shell=True)
     zip_result()
 
-    subprocess.run(f"cd {PROJECT_ROOT} && make dpurge", shell=True)
-
     move_file("./tools/config_py/config.py", "config.py")
+
+    subprocess.run(f"cd {PROJECT_ROOT} && make dpurge", shell=True)
     subprocess.run("python3 main.py", shell=True)
     zip_result()
