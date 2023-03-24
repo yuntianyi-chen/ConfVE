9ce11e0da5116ac72b24ec3ef5b6b636d899213b
yuntianyi-chen <yuntianyichen@outlook.com>
2023-03-24 16:43:41 -0700

    modify zip_result.py for convenient analysis
    modify zip_result.py
    fix zip bug in run_main.py

--- src/run_main.py
+++ src/run_main.py
@@ -2,10 +2,7 @@ import os
 import shutil
 import subprocess
 import warnings
-
 from config import PROJECT_ROOT
-from main import confve_main
-from tools.script.zip_result import zip_result
 
 warnings.filterwarnings('ignore')
 
@@ -19,10 +16,10 @@ def move_file(source_dir, target_dir):
 if __name__ == '__main__':
     subprocess.run(f"cd {PROJECT_ROOT} && make dpurge", shell=True)
     subprocess.run("python3 main.py", shell=True)
-    zip_result()
+    subprocess.run("python3 tools/script/zip_result.py", shell=True)
 
     move_file("./tools/config_py/config.py", "config.py")
 
     subprocess.run(f"cd {PROJECT_ROOT} && make dpurge", shell=True)
     subprocess.run("python3 main.py", shell=True)
-    zip_result()
+    subprocess.run("python3 tools/script/zip_result.py", shell=True)
